d0b2306423b0156299386ca4050f8445e85a22cd
Jonathan Kim <kimgiutae@gmail.com>
2024-02-06 11:37:14 -0300

    chore: update deps and some configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./src/**/*.{ts,tsx}'],
+  content: ['./src/components/*.tsx', './src/app/**/*.tsx'],
   theme: {
     screens: {
       xs: '400px',
